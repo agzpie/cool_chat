4aba4bd62b9bfa9d57f821f69fc563f5a63d2731
Agata Pietralik <agata.pietralik@student.wat.edu.pl>
2021-05-06 16:00:30 +0200

    I guess it is done, it could be better though

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React, { Component, useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { Avatar, Box, Button, Form, Formfield, Grid, grommet, Grommet, Heading, Image, List, Nav, Sidebar, TextArea, TextInput } from 'grommet';
+import { Avatar, Box, Button, grommet, Grommet, Heading, Image, List, Nav, TextInput } from 'grommet';
 import { Chat, Github, Help, Menu, Next, Notification, Phone, Plan } from 'grommet-icons';
-import { render } from '@testing-library/react';
 import Logo from "./logo.png"
 
 /*
@@ -13,7 +11,7 @@ import Logo from "./logo.png"
 reportWebVitals();
 */
 
-// MESSAGE 
+// Message input field
 class InputField extends Component {
   constructor(props) {
     super(props)
@@ -21,7 +19,7 @@ class InputField extends Component {
       writeMessage: "",
       responseMessage: "",
       isLoading: true,
-      error: ""
+      error: "",
     }
   }
 
@@ -36,7 +34,7 @@ class InputField extends Component {
     event.preventDefault()
     console.log(event.target.name)
     this.setState({
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     })
   }
 
@@ -56,20 +54,20 @@ class InputField extends Component {
       .then(result => {
         this.setState({
           isLoading: false,
-          responseMessage: result.ok
+          responseMessage: result.ok,
         });
       },
         (error) => {
           this.setState({
             isLoading: true,
-            error: error
+            error: error,
           });
         })
       .catch(err => {
         console.log('err', err);
         this.setState({
           isLoading: true,
-          error: err
+          error: err,
         })
       });
   }
@@ -79,25 +77,25 @@ class InputField extends Component {
       <form onSubmit={this.handleSubmit}>
         <Box direction='row' pad='15' margin='small' gap='small'>
           <TextInput
-          id="text-input"
-          name='writeMessage'
-          background='white'
-          value={this.state.value}
-          onChange={this.handleInputChange}
-          resize={false}
-          placeholder="write message..."
+            id="text-input"
+            name='writeMessage'
+            background='white'
+            value={this.state.value}
+            onChange={this.handleInputChange}
+            resize={false}
+            placeholder="write message..."
           />
           <Button icon={<Next />} hoverIndicator type='submit' onClick={this.handleSubmit} />
         </Box>
       </form>
-    )
+    );
   }
 }
 
 const Input = () => (
-<Box border='top'>
-  <InputField />
-</Box>
+  <Box border='top'>
+    <InputField />
+  </Box>
 )
 
 class UsersSidebar extends Component {
@@ -124,126 +122,120 @@ class UsersSidebar extends Component {
   }
 
   render() {
-    return(     
+    return (
       <Box pad='small'>
-      <List
-        data={this.state.users.slice()}
-      /> 
-      </Box>    
+        <List
+          data={this.state.users.slice()}
+        />
+      </Box>
     )
   }
 }
 
 const RightSidebar = () => (
-    <Box width='small' align='center' justify='start'>
-      <Heading level='3' color='#8abaa0'>users</Heading>
-      <UsersSidebar />
-    </Box>
+  <Box width='small' align='center' justify='start'>
+    <Heading level='3' color='#8abaa0'>users</Heading>
+    <UsersSidebar />
+  </Box>
 )
 
-class MessagesField extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      isLoaded: false,
-    }
-  }
 
-  // API
-  componentDidMount() {
+  // Fetching messages from the API periodically - every 5 seconds
+function MessagesUpdater() {
+  const [posts, setPosts] = useState([])
+  const getPosts = async () => {
     fetch('https://608947d48c8043001757e674.mockapi.io/messages')
       .then(res => res.json())
       .then(json => {
-        this.setState({
-          items: json.messages,
-          isLoaded: true,
-        })
+        setPosts(json.messages)
       }).catch((err) => {
         console.log(err);
       });
-  }
+  };
 
-  render() {
+  useEffect(() => {
+    getPosts()
+    const interval = setInterval(() => {
+      getPosts()
+    }, 5000)
+    return () => clearInterval(interval)
+  }, [])
 
-    const abcd = ['a', 'b']
-    return( 
-      <Box pad='small'>
-      <List
-        data={this.state.items.slice()}
-        primaryKey='abcd'
-      /> 
-
-      
-      
+  return (
+    <Box pad='small' align='stretch' height='medium' alignContent='stretch'>
       <ul>
-      {this.state.items.map(item => (
-        <li key={item.timestamp}>
-          USER: {item.user} | MESSAGE: {item.text}
-        </li>
-      ))}
+        {posts.map(post => (
+          <li key={post.user}>
+            USER: {post.user} | MESSAGE: {post.text}
+          </li>
+        ))}
       </ul>
-      </Box>
-    )
-  }
+    </Box>
+  );
 }
 
+// Messages field
 const RecentMessages = () => (
-    <Box direction='column' align='center' justify='start' background='light-3' border='vertical'>
-      <Box height='large'>
-        <Heading level='3' color='#8abaa0'> recent messages </Heading>
-      </Box>
-      <MessagesField />
+  <Box direction='column'  align='center' justify='start' background='light-3' border='vertical'>
+    <Box>
+      <Heading level='3' color='#8abaa0'> recent messages </Heading>
     </Box>
+    <MessagesUpdater />
+    <Box align='end' alignSelf='end' pad='small'>
+        <Heading level='6'>messages update every 5s </Heading>
+      </Box>
+  </Box>
 )
 
+// AppBar is the header
 const AppBar = (props) => (
-    <Box
-      tag='header'
-      direction='row'
-      align='center'
-      justify='between'
-      background="linear-gradient(102.77deg, #8aafba -9.18%, #8abaa0 209.09%)"
-      pad={{ left: 'medium', right: 'small', vertical: 'small' }}
-      {...props}
-    >
-      <Image src={Logo} alt='a cool chat logo' />
-      <Button icon={<Menu color='white' />} hoverIndicator/>
-    </Box>
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background="linear-gradient(102.77deg, #8aafba -9.18%, #8abaa0 209.09%)"
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    {...props}
+  >
+    <Image src={Logo} alt='a cool chat logo' />
+    <Button icon={<Menu color='white' />} hoverIndicator />
+  </Box>
 );
 
 const LeftSidebar = () => (
-    <Box direction='column' pad='small' gap='small'>
-      <Box>
-        <Avatar src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />
-      </Box>
-      <Box flex='1' border={{ color: '#8abaa0', side: 'horizontal' }}>
-        <Nav gap="small">
-          <Button icon={<Notification />} hoverIndicator />
-          <Button icon={<Chat />} hoverIndicator />
-          <Button icon={<Phone />} hoverIndicator />
-          <Button icon={<Plan />} hoverIndicator />
-          <Button icon={<Github />} hoverIndicator />
-          <Button icon={<Help />} hoverIndicator />
-        </Nav>
-      </Box>
+  <Box direction='column' pad='small' gap='small'>
+    <Box>
+      <Avatar src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />
     </Box>
+    <Box border={{ color: '#8abaa0', side: 'horizontal' }}>
+      <Nav gap="small">
+        <Button icon={<Notification />} hoverIndicator />
+        <Button icon={<Chat />} hoverIndicator />
+        <Button icon={<Phone />} hoverIndicator />
+        <Button icon={<Plan />} hoverIndicator />
+        <a href='https://github.com/agzpie/cool_chat'>
+          <Button icon={<Github />} hoverIndicator />
+        </a>
+        <Button icon={<Help />} hoverIndicator />
+      </Nav>
+    </Box>
+  </Box>
 )
 
 class App extends React.Component {
 
   render() {
-
     return (
       <Grommet theme={grommet} full>
         <AppBar />
         <Box direction='row' basis='auto' fill='true' justify='stretch' >
           <LeftSidebar />
-            <Box direction='column' fill='true' flex='1'>
+          <Box direction='column' fill='true'>
             <RecentMessages />
             <Input />
           </Box>
-          <Box fill='vertical' flex='1'>
+          <Box fill='vertical'>
             <RightSidebar />
           </Box>
         </Box>
